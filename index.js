9a8fe3c0a95f134c13a7e9800eccafe164e8a820
qojqvax <56183442+qojqvax@users.noreply.github.com>
2025-08-01 23:57:15 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const admin = require('firebase-admin');
-const serviceAccount = require('./serviceAccountKey.json');
+const serviceAccount = JSON.parse(process.env.SERVICE_ACCOUNT);
 const authUser = 'riaLogistic';
 const authPass = 'notiLogisticRia';
 
